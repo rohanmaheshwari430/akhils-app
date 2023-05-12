cc5656e9c25d1e5aee2148c435f2ec3700e715f5
Rohan Maheshwari <rohanm@gmail.com>
2023-05-12 16:26:50 -0700

    Set up app

---
+++ my-app/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Akhil's App!
+        </p>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <p> {count} </p>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://nba.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Basketball
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
