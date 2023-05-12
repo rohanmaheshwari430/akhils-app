f811b9b5c63152128da08e7da162f2defddd3dc5
Rohan Maheshwari <rohanm@gmail.com>
2023-05-12 16:27:50 -0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Akhil's App!
+          Welcome to Nikhils's App!
         </p>
         <button onClick={() => setCount(count + 1)}>Increase</button>
         <p> {count} </p>
